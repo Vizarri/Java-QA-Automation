c50f4317247a2ab40b34a3adafbd880dbbb4dfe7
Vizarri <82329177+Vizarri@users.noreply.github.com>
2021-08-06 00:18:44 +0400

    Update testElevatorAreTheDoorsOpen.java

--- src/test/java/Tests/testElevatorAreTheDoorsOpen.java
+++ src/test/java/Tests/testElevatorAreTheDoorsOpen.java
@@ -4,12 +4,15 @@ import elevator.ElevatorFloors;
 import models.Elevator.Elevator;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class testElevatorAreTheDoorsOpen {
-    @Test
-    public void testElevatorAreTheDoorsOpen() {
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void testElevatorAreTheDoorsOpen(boolean doorstatus) {
         ElevatorFloors elevatorFloors = new ElevatorFloors(-1, 5);
-        Elevator elevator = new Elevator(1, elevatorFloors, true);
+        Elevator elevator = new Elevator(1, elevatorFloors, doorstatus);
         elevator.moveElevatorToTheFloor(3);
         int expectedResult = 1;
         int actualResult = elevator.getCurrentFloor();
