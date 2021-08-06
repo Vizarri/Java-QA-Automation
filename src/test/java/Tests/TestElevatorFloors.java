235bc5be61ad77b9b0a34d318ef29fab4f40f513
Vizarri <vizarri1999@gmail.com>
2021-08-06 13:49:11 +0400

    review

--- src/test/java/Tests/TestElevatorFloors.java
+++ src/test/java/Tests/TestElevatorFloors.java
@@ -1,16 +1,15 @@
 package Tests;
 
 import elevator.ElevatorFloors;
-import elevator.exceptions.IncorrectFloorException;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class testElevatorFloorsGetMinFloor {
+public class TestElevatorFloors {
 
     @Test
     public void testElevatorFloorsСonstructor() {
         ElevatorFloors elevatorFloors = new ElevatorFloors(5, 10);
-        int expectedResult = 0;
+        int expectedResult = 5;
         int actualResult = elevatorFloors.getMinFloor();
         Assert.assertEquals("Минимальный этаж не может быть больше 1", expectedResult, actualResult);
 
