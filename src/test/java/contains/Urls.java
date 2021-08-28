4e048fedfc47bb85c784b2892751275273fbb5c0
Vizarri <vizarri1999@gmail.com>
2021-08-29 00:52:24 +0300

    API

--- src/test/java/contains/Urls.java
+++ src/test/java/contains/Urls.java
@@ -1,5 +1,5 @@
 package contains;
 
 public class Urls {
-    final static public String ACTION_URL = "https://store.steampowered.com/";
+    final static public String HTTPBIN_URL = "https://httpbin.org";
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,19 +1,16 @@
 package tests;
 
-import com.codeborne.selenide.Selenide;
 import contains.Urls;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 
 public class BaseTest {
+    protected RequestSpecification rspec;
+    protected RequestSpecBuilder build;
 
-    @BeforeEach
-    public void setUp() {
-        Selenide.open(Urls.ACTION_URL);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        Selenide.closeWebDriver();
+    protected BaseTest() {
+        build = new RequestSpecBuilder();
+        build.setBaseUri(Urls.HTTPBIN_URL);
+        rspec = build.build();
     }
 }
